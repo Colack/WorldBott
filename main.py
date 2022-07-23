ed2be924654310295a4e37b2718d6f309212d421
Jack Spencer <UniformKitten281@gmail.com>
2022-07-23 15:23:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import requests
 
 username = ""
 password = ""
-previous = ""
+
 version = "1.2"
+
 default_comment = "Randomized message created by (@WorldBott). Contact (@colack) if you want to suggest a new message. (Or Template!)"
 default_status = "Hi There! I'm WorldBott!\nI'm a bot created by (@colack).\nI use DeepAI and a lot of pre-generated messages to create random status updates.\n\nContact (@colack) if you want to suggest a new message.\n\nI'm currently running version " + version + "."
+default_message = ":)"
+
 prompts = [
     "Hi there, I'm WorldBott!",
     "I'm WorldBott!",
@@ -29,25 +32,43 @@ def like():
     pyautogui.moveTo(775, 300, duration = 1)
     pyautogui.click(775, 300)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Move to the like button and click it.
     pyautogui.moveTo(775, 730, duration = 1)
     pyautogui.click(775, 730)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Move to the comment box and click it.
     pyautogui.moveTo(1140, 730, duration=1)
     pyautogui.click(1140, 730)
+
+    # Wait a bit 
+    time.sleep(2.5)
 
     # Move to the comment field and type the default comment.
     pyautogui.moveTo(1140, 700, duration = 1)
     pyautogui.click(1140, 700)
     pyautogui.typewrite(default_comment)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Type "ENTER" to submit the comment.
     pyautogui.press('enter')
+
+    # Wait a bit 
+    time.sleep(2.5)
 
     # Move to the exit button and click it.
     pyautogui.moveTo(775, 200, duration = 1)
     pyautogui.click(775, 200)
+
+    # Wait a bit 
+    time.sleep(2.5)
 
 # Generate a new status
 def statusChange():
@@ -55,11 +76,67 @@ def statusChange():
     pyautogui.moveTo(1050, 745, duration=1)
     pyautogui.click(1050, 745)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Move to the edit button and click it.
     pyautogui.moveTo(800, 310, duration = 1)
     pyautogui.click(800, 310)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Move to the text box and click it.
+    pyautogui.moveTo(1032, 280, duration = 1)
+    pyautogui.click(1032, 280)
+
+    # Wait a bit 
+    time.sleep(2.5)
+
+    # Type "CTRL + A" to select all the text.
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('a')
+
+    # Wait a bit 
+    time.sleep(2.5)
+    
+    # Type "DELETE" to delete the text.
+    pyautogui.keyUp('ctrl')
+    pyautogui.press('delete')
+
+    # Wait a bit 
+    time.sleep(2.5)
+
+    # Update the status to the default status.
+    pyautogui.typewrite(default_status)
+
+    # Wait a bit 
+    time.sleep(2.5)
+
+    # Move to the save button and click it.
+    pyautogui.moveTo(1100, 730, duration=1)
+    pyautogui.click(1100, 730)
+
+    # Wait a bit 
+    time.sleep(2.5)
+
+# Start and Login
+def start():
+    # Print the name and version of the bot.
+    print("WorldBott " + version + " is starting...")
+
+    # Close visual studio code
+    pyautogui.moveTo(1760, 0, duration = 1)
+    pyautogui.click(1760, 0)
+
+    # Reload the page
+    pyautogui.keyDown("CTRL")
+    pyautogui.press("R")
+
+    # Wait a bit for the page to reload
+    time.sleep(10)
+
+    # Move to the username field
     pyautogui.moveTo(1032, 280, duration = 1)
     pyautogui.click(1032, 280)
 
@@ -71,32 +148,20 @@ def statusChange():
     pyautogui.keyUp('ctrl')
     pyautogui.press('delete')
 
-    # Update the status to the default status.
-    pyautogui.typewrite(default_status)
-
-    # Move to the save button and click it.
-    pyautogui.moveTo(1100, 730, duration=1)
-    pyautogui.click(1100, 730)
-
-# Start and Login
-def start():
-    # Print the name and version of the bot.
-    print("WorldBott " + version + " is starting...")
-
-    # Close visual studio code
-    pyautogui.moveTo(1760, 0, duration = 1)
-    pyautogui.click(1760, 0)
-
-    # Move to the username field
-    pyautogui.moveTo(1032, 280, duration = 1)
-    pyautogui.click(1032, 280)
-
     # Type the username
     pyautogui.typewrite(username)
 
     # Move to the password field
     pyautogui.moveTo(1032, 325, duration = 1)
     pyautogui.click(1032, 325)
+
+    # Type "CTRL + A" to select all the text.
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('a')
+    
+    # Type "DELETE" to delete the text.
+    pyautogui.keyUp('ctrl')
+    pyautogui.press('delete')
 
     # Type the password
     pyautogui.typewrite(password)
@@ -119,55 +184,61 @@ def main():
     pyautogui.click(965, 730)
     pyautogui.click(965, 730)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Go to the message area.
     pyautogui.moveTo(1032, 280, duration = 1)
     pyautogui.click(1032, 280)
 
-    # Wait a couple of seconds ~5
-    time.sleep(5)
+    # Wait a bit 
+    time.sleep(2.5)
 
-    # Make sure that the message is not the same as the previous message.
-    while message == previous:
-        # Create a random number between 1 and 5.
-        random_number = random.randint(1, 10)
+    # Create a random number between 1 and 5.
+    random_number = random.randint(1, 10)
     
-        if random_number == 1:
-            # Create a random string of numbers and letters
-            random_string = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(1, 10)))
-            message = random_string + " <-- My new favorite word!"
-            previous = message
-        elif random_number == 2:
-            message = "I'm a bot created by (@colack)"
-            previous = message
-        elif random_number == 3:
-            # Grab a random dad joke of the internet.
-            url = "https://icanhazdadjoke.com/"
-            responce = requests.get(url, headers={"Accept": "application/json"})
-            message = responce.json()['joke']
-            previous = message
-        elif random_number == 4:
-            message = "✌(ツ)"
-            previous = message
-        elif random_number == 5:
-            message = "🤖"
-            previous = message
-        elif random_number == 6:
-            message = "Don't forget, if you want to suggest a new message, contact (@colack)!"
-            previous = message
-        else:
-            # Grab a random prompt and send it to the deep ai model.
-            r = requests.post(
-                "https://api.deepai.org/api/text-generator",
-            data={
-                'text': random.choice(prompts),
-            },
-                headers={'api-key': ''}
-            )
-            message = r.json()['output']
-            previous = message
+    previous = ""
+
+    if (previous == ""):
+        # If the previous message was empty, use the default message.
+        previous = default_message
+
+    if random_number == 1:
+        # Create a random string of numbers and letters
+        random_string = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(1, 10)))
+        message = random_string + " <-- My new favorite word!"
+    elif random_number == 2:
+        message = "I'm a bot created by (@colack)"
+    elif random_number == 3:
+        # Grab a random dad joke of the internet.
+        url = "https://icanhazdadjoke.com/"
+        responce = requests.get(url, headers={"Accept": "application/json"})
+        message = responce.json()['joke']
+    elif random_number == 4:
+        message = "✌(ツ)"
+    elif random_number == 5:
+        message = "🤖"
+    elif random_number == 6:
+        message = "Don't forget, if you want to suggest a new message, contact (@colack)!"
+    else:
+        # Grab a random prompt and send it to the deep ai model.
+        r = requests.post(
+            "https://api.deepai.org/api/text-generator",
+        data={
+            'text': random.choice(prompts),
+        },
+            headers={'api-key': ''}
+        )
+        message = r.json()['output']
+
+    if (message == previous):
+        message = "I'm sorry, I couldn't find a new message for you."
 
     # Message is now created.
     print("Creating new message...")
+
+    # This message is now the previous message.
+    previous = message
     
     # We will now make sure that the message is not too long.
     if len(message) > 140:
@@ -186,13 +257,22 @@ def main():
     else:
         # If the message is not too short, we will leave it as is.
         pass
+
+    # Wait a couple of seconds ~5
+    time.sleep(5)
     
     # Type the message    
     pyautogui.typewrite(message)
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Hit the post button
     pyautogui.moveTo(1100, 400, duration = 1)
     pyautogui.click(1100, 400)
+
+    # Wait a bit 
+    time.sleep(2.5)
 
     # Wait a bit for the message to be posted.
     time.sleep(10)
@@ -200,8 +280,14 @@ def main():
     # Like and comment on the message.
     like()
 
+    # Wait a bit 
+    time.sleep(2.5)
+
     # Change the current status to a new status.
     statusChange()
+
+    # Wait a bit 
+    time.sleep(2.5)
 
     # Wait 24 hours to post again.
     time.sleep(86400)
