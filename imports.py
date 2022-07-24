aa9a2b1507b5e2e81586d8971cd9e08eca8c5e84
Jack Spencer <UniformKitten281@gmail.com>
2022-07-24 16:16:28 -0700

    Update imports.py

--- imports.py
+++ imports.py
@@ -8,5 +8,24 @@ import sys
 import subprocess
 import threading
 import PySimpleGUI as sg
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import tkinter.filedialog as tkFileDialog
+import tkinter.simpledialog as tkSimpleDialog
+import tkinter.colorchooser as tkColorChooser
+import tkinter.font as tkFont
+import tkinter.scrolledtext as tkScrolledText
+import array
+import argparse
+import csv
+import zipfile
+import shutil
+import urllib.request
+import urllib.parse
+import urllib.error
+import unicodedata
+import webbrowser
 
 # This file contains all of the libraries that are used in the bot.
