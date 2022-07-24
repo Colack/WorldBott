701da5d69647b520ada3f3578c7da9926abe682c
Jack Spencer <UniformKitten281@gmail.com>
2022-07-23 20:46:21 -0700

    Create imports.py

---
+++ imports.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+import string
+import random
+import requests
+import os
+import sys
+import subprocess
+import threading
+
+# This file contains all of the libraries that are used in the bot.
