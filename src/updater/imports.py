9fefe25c22ad591b6ecf9d8c8c35674c40b81bc1
Jack Spencer <UniformKitten281@gmail.com>
2022-07-25 14:13:47 -0700

    Create imports.py

---
+++ src/updater/imports.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+import string
+import random
+import requests
+import os
+import sys
+import subprocess
+import threading
+import PySimpleGUI as sg
+from dotenv import load_dotenv
+
+# This file contains all of the libraries that are used in the Program.
